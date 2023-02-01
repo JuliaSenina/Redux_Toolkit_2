2392b00e8ec415e02054a484e1ec88d2430280f9
Сенина Юлия Александровна <y.senina@ens.ru>
2023-02-01 12:14:02 +0300

    commit

---
+++ toolkit/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import fileSlice from "../slice/fileSlice";
+
+export const store = configureStore({
+  reducer: {
+    file: fileSlice,
+  },
+});
